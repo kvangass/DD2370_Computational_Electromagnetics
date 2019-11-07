717340e3f348125eab72c07c7ea3e39e362b12b2
Kwinten <vangassen.k@gmail.com>
2019-11-07 14:10:22 +0100

    Task1 finite difference working

--- Finite_Difference/Task1.m
+++ Finite_Difference/Task1.m
@@ -1,4 +1,18 @@
 %Task 1
 close all; clear all
-capacitor_FD(0.5,0.5,2,2,100,1E-3,1)
 
+n_vals = 5:5:100;
+cap_vals = zeros(length(n_vals),1);
+i=0;
+for n = n_vals
+    i = i+1;
+    cap_1 = capacitor_FD2(0.5,0.5,2,2,n,1E-3,1);
+    cap_vals(i) = cap_1;
+end
+%cap_2 = capacitor(0.5,0.5,2,2,30,1E-3,1)
+
+figure(3)
+plot(n_vals,cap_vals)
+xlabel('n')
+ylabel('cap')
+title('Convergence')
